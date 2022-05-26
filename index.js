7ea9e37eae8ee3f487d6b14af3f7fc937d917a8e
Majba <monaiburkhan@gmail.com>
2022-05-26 22:27:21 +0600

    admin

--- index.js
+++ index.js
@@ -108,8 +108,9 @@ app.put('/user/admin/:email', async (req, res) => {
     const result = await userCollection.updateOne(filter, updateDoc);
     res.send(result);
     }
-
-
+    else{
+        res.status(403).send({ message: 'forbidden'});
+    }
     
     
   })
@@ -213,7 +214,16 @@ app.put('/user/admin/:email', async (req, res) => {
 
             res.send(result);
 
-        })
+        });
+
+        //make an admin verify page
+    app.get('/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        const isAdmin = user.role === 'admin';
+        res.send({ admin: isAdmin });
+  
+      })
 
 
 
